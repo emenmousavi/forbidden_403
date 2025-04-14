39367686389e6dc077362dce99980c6bff4cbe2e
Emen M <108234550+emenmousavi@users.noreply.github.com>
2025-04-14 23:03:43 +0400

    Update fix_403.sh

--- fix_403.sh
+++ fix_403.sh
@@ -6,23 +6,19 @@ yellow="\e[33m"
 blue="\e[34m"
 reset="\e[0m"
 
-# Trap Ctrl+C
 trap 'echo -e "\n${red}❌ Script interrupted. Exiting.${reset}"; exit 1' INT
 
 echo -e "${blue}🚀 Forbidden 403 Auto-Fix Script By Emen Mousavi${reset}"
 
-# User inputs
 read -p "🌐 Enter the SFTP host: " host
 read -p "📦 Enter the SFTP port [default 55100]: " port
 port=${port:-55100}
 read -p "👤 Enter the SFTP username: " username
 
-# Remote file and directory setup
 remote_dir="/home/wplive/web/wp-live"
 remote_file="index.php"
 raw_url="https://raw.githubusercontent.com/emenmousavi/forbidden_403/main/index.php"
 
-# Check if index.php exists
 echo -e "${yellow}🔍 Checking if $remote_file exists on the server...${reset}"
                                                    [ Read 125 lines ]
 ^G Help        ^O Write Out   ^F Where Is    ^K Cut         ^T Execute     ^C Location    M-U Undo       M-A Set Mark
